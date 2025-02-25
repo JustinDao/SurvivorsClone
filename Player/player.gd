da2104adaece1faf4d2d1232104c82b74dba680d
JDao <justin.dao@mothership.com>
2025-02-25 14:21:26 -0800

    Tornado attack

--- Player/player.gd
+++ Player/player.gd
@@ -2,19 +2,29 @@ extends CharacterBody2D
 
 var movement_speed: float = 80.0
 var hp: float = 80.0
+var last_movement: Vector2 = Vector2.UP
 
 # Attacks
 var ice_spear = preload("res://Player/Attack/ice_spear.tscn")
+var tornado = preload("res://Player/Attack/tornado.tscn")
 
 # AttackNodes
 @onready var ice_spear_timer = $Attack/IceSpearTimer
 @onready var ice_spear_attack_timer = $Attack/IceSpearTimer/IceSpearAttackTimer
+@onready var tornado_timer = $Attack/TornadoTimer
+@onready var tornado_attack_timer = $Attack/TornadoTimer/TornadoAttackTimer
 
 # IceSpear
 var ice_spear_ammo = 0
 var ice_spear_base_ammo = 1
 var ice_spear_attack_speed = 1.5
-var ice_spear_level = 1
+var ice_spear_level = 0
+
+# Tornado
+var tornado_ammo = 0
+var tornado_base_ammo = 3
+var tornado_attack_speed = 3
+var tornado_level = 1
 
 # Enemies
 var enemy_close = []
@@ -39,6 +49,7 @@ func movement() -> void:
 		sprite.flip_h = false
 
 	if mov != Vector2.ZERO:
+		last_movement = mov
 		if walkTimer.is_stopped():
 			if sprite.frame >= sprite.hframes - 1:
 				sprite.frame = 0
@@ -54,12 +65,34 @@ func movement() -> void:
 func _on_hurt_box_hurt(damage: float, _angle: Vector2, _knockback: float) -> void:
 	hp -= damage
 
+func get_random_target() -> Vector2:
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+
+	return Vector2.UP
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if !enemy_close.has(body):
+		enemy_close.append(body)
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
+
+
+# Attacks
 func attack() -> void:
 	if ice_spear_level > 0:
 		ice_spear_timer.wait_time = ice_spear_attack_speed
 		if ice_spear_timer.is_stopped():
 			ice_spear_timer.start()
+	
+	if tornado_level > 0:
+		tornado_timer.wait_time = tornado_attack_speed
+		if tornado_timer.is_stopped():
+			tornado_timer.start()
 
+# IceSpear Timers
 func _on_ice_spear_timer_timeout() -> void:
 	ice_spear_ammo += ice_spear_base_ammo
 	ice_spear_attack_timer.start()
@@ -77,16 +110,20 @@ func _on_ice_spear_attack_timer_timeout() -> void:
 		else:
 			ice_spear_attack_timer.stop()
 
-func get_random_target() -> Vector2:
-	if enemy_close.size() > 0:
-		return enemy_close.pick_random().global_position
+# Tornado Timers
+func _on_tornado_timer_timeout() -> void:
+	tornado_ammo += tornado_base_ammo
+	tornado_attack_timer.start()
 
-	return Vector2.UP
-
-func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
-	if !enemy_close.has(body):
-		enemy_close.append(body)
-
-func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
-	if enemy_close.has(body):
-		enemy_close.erase(body)
+func _on_tornado_attack_timer_timeout() -> void:
+	if tornado_ammo > 0:
+		var new_tornado = tornado.instantiate()
+		new_tornado.position = position
+		new_tornado.last_movement = last_movement
+		new_tornado.level = tornado_level
+		add_child(new_tornado)
+		tornado_ammo -= 1
+		if tornado_ammo > 0:
+			tornado_attack_timer.start()
+		else:
+			tornado_attack_timer.stop()
